be1e35dfa200c8a5c2ec5cff55da77c8272329f8
Quint Guvernator <quint@guvernator.net>
2022-12-01 15:45:20 +0100

    fewer work commits, one extra diagram

--- create_history.sh
+++ create_history.sh
@@ -32,6 +32,7 @@ git tag -am v4.0.0 v4.0.0
 
 git switch develop
 git commit --allow-empty -m 'work 1'
+git cherry-pick --allow-empty v4.0.0
 git commit --allow-empty -m 'work 2'
 git commit --allow-empty -m 'update translations 2'
 
@@ -41,11 +42,11 @@ git tag -am v4.1.0 v4.1.0
 
 git switch develop
 git commit --allow-empty -m 'work 3'
+git cherry-pick --allow-empty v4.1.0
 git commit --allow-empty -m 'work 4'
 git commit --allow-empty -m 'update translations 3'
 git branch uat
 git commit --allow-empty -m 'work 5'
-git commit --allow-empty -m 'work 6'
 
 diagram
 
@@ -53,8 +54,7 @@ git switch uat
 git commit --allow-empty -m 'hotfix 1'
 
 git switch develop
-git commit --allow-empty -m 'work 7'
-git commit --allow-empty -m 'work 8'
+git commit --allow-empty -m 'work 6'
 
 diagram
 
@@ -62,8 +62,7 @@ git cherry-pick --allow-empty uat
 
 diagram
 
-git commit --allow-empty -m 'work 9'
-git commit --allow-empty -m 'work 10'
+git commit --allow-empty -m 'work 7'
 
 diagram
 
@@ -89,8 +88,13 @@ git tag -am v4.2.0 v4.2.0
 
 diagram
 
-# update translations on develop
+# cherry-pick release commit
 git switch develop
+git cherry-pick --allow-empty v4.2.0
+
+diagram
+
+# update translations on develop
 git commit --allow-empty -m 'update translations 4'
 
 diagram
@@ -101,8 +105,7 @@ git branch -f uat develop
 diagram
 
 # now, develop can move on
-git commit --allow-empty -m 'work 11'
-git commit --allow-empty -m 'work 12'
+git commit --allow-empty -m 'work 8'
 
 diagram
 
